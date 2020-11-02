2f7fdf1f3e0baa85c91ed2b4a72d61b7396deb94
Justin Wasserstein <justin.wasserstein@gmail.com>
2020-11-02 00:06:59 +0000

    Changed mongodb uri to process.env.MONGODB_URI || locahost and added Procfile

--- models/index.js
+++ models/index.js
@@ -4,6 +4,6 @@ const Message  = require('./message');
 
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/warbler', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/warbler', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, keepAlive: true});
 
 module.exports = { User, Message };
